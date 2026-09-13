dfc3489e7621df1c133e0bb095ebc24854a26c44
Apodixis <238086693+Apodixis@users.noreply.github.com>
2026-09-13 15:54:58 -0400

    docs: changed enrichment_prompt() affirmative print statement to indicate that enrichment may take awhile (necessary distinction due to some search methods yielding large results sets).

--- Utils/menus.py
+++ Utils/menus.py
@@ -85,7 +85,7 @@ def enrichment_prompt() -> str:
     Output: Returns the user's choice as a string ("1" or "2")
     """
     clear_terminal()
-    print("1) Enrich Results: Will take longer") # Additional calls to enrichment functions and HTTP response delays
+    print("1) Enrich Results: May take significantly longer") # Additional calls to enrichment functions and HTTP response delays
     print("2) Skip Enrichment") # Skips the enrichment step
     
     while True:
